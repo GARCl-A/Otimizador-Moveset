ebaa85610a6cf9a1ffb3359afc8f71bed49fc943
Lucas Garcia <lucasgarcia@poli.ufrj.br>
2026-05-08 05:00:20 -0300

    legends and upgrades

--- typescript/app/src/engine/lendarios.ts
+++ typescript/app/src/engine/lendarios.ts
@@ -1,4 +1,7 @@
 export const LENDARIOS = [
   'Mewtwo', 'Rayquaza', 'Deoxys', 'Latios', 'Palkia', 'Cresselia',
   'Victini', 'Hoopa', 'Tapu Fini', 'Buzzwole', 'Eternatus', 'Munkidori', 'Zacian', 'Zamazenta', 'Eternatus', 'Kubfu', 'Urshifu', 'Regieleki', 'Regidrago',
+  'Reshiram', 'Zekrom', 'Kyurem', 'Xerneas', 'Yveltal', 'Ho-Oh', 'Lugia', 'Groudon', 'Kyogre', 'Dialga', 'Giratina', 'Arceus', 'Zygarde', 'Necrozma', 'Zacian', 'Zamazenta',
+  'Heatran', 'Regigigas', 'Cobalion', 'Terrakion', 'Virizion', 'Tornadus', 'Thundurus', 'Landorus', 'Entei', 'Raikou', 'Suicune', 'Celebi', 'Jirachi', 'Deoxys', 'Manaphy', 'Darkrai', 'Shaymin', 'Arceus', 'Victini', 'Keldeo', 'Meloetta', 'Genesect',
+  'Diancie', 'Hoopa', 'Volcanion', 'Magearna', 'Marshadow', 'Zeraora', 'Meltan', 'Melmetal'
 ];
